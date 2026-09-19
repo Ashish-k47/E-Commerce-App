c6d993d1a367da09fd9fa95dbf20b23884dc744d
Ashish Kushwaha <176682890+Ashish-k47@users.noreply.github.com>
2026-09-19 23:14:36 +0530

    woked on [id].tsx

--- client/app/product/[id].tsx
+++ client/app/product/[id].tsx
@@ -1,15 +1,36 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import React, { useEffect, useState } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { Product } from '../../constants/types';
+import { useCart } from '../../context/CartContext';
+import { useWishlist } from '../../context/WishlistContext';
+import { dummyProducts } from '../../assets/assets';
 
 export default function ProductDetails() {
 
     const {id} = useLocalSearchParams()
+    const router = useRouter();
+    const [product, setProduct] = useState<Product | null>(null)
+    const [loading, setLoading] = useState(true)
 
-    
+    const {addToCart, cartItem} = useCart()
+    const {toggleWishlist, isInWishlist} = useWishlist()
+
+    const [slectedSize, setSlectedSize] = useState<string | null>(null)
+    const [activeImageIndex, setActiveImageIndex] = useState(0)
+
+    const fetchProduct = async () =>{
+      setProduct(dummyProducts.find((product) => product._id === id) as any)
+      setLoading(false)
+    }
+
+    useEffect(()=>{
+      fetchProduct()
+    },[])
+
   return (
     <View>
-      <Text>Produc tDetails</Text>
+      <Text>[id]</Text>
     </View>
   )
 }
